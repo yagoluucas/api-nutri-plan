2ad857686b98ee0b9aad09323502ce0589ee849c
yagoluucas <yago543@gmail.com>
2026-05-13 19:58:55 -0300

    fix: Adjust IUsuarioSchema for add a validation properties

--- src/interfaces/usuarios/usuarioInterfaces.ts
+++ src/interfaces/usuarios/usuarioInterfaces.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 const IUsuarioSchema = z.object({
-    nome: z.string(),
-    sobrenome: z.string(),
-    email: z.email(),
+    nome: z.string().trim().min(2),
+    sobrenome: z.string().trim().min(2),
+    email: z.email().trim().toLowerCase().min(5).max(60),
     dataNascimento: z.coerce.date(),
-    senhaHash: z.string()
+    senha: z.string().min(8).max(20)
 });
 
 type IUsuario = z.infer<typeof IUsuarioSchema>;
