e4d4a5907423e3334568c36f156c77045ec8b2e0
Yago Lucas Gonçalves da Silva <120147089+yagoluucas@users.noreply.github.com>
2026-07-11 15:32:22 -0300

    feat: encrypt and decrypt diet plan payloads

--- src/modules/planoAlimentar/planoAlimentarHelpers.ts
+++ src/modules/planoAlimentar/planoAlimentarHelpers.ts
@@ -2,42 +2,90 @@ import { NextFunction } from "express";
 import { IErrorCause } from "../../interfaces/errors/erros.js";
 import {
   IPlanoAlimentar,
+  IPlanoAlimentarPersistido,
   IPlanoAlimentarSchema,
   IPlanoAlimentarRetornoSchema,
 } from "../../interfaces/planoAlimentar/planoAlimentarInterfaces.js";
+import {
+  decryptJson,
+  encryptJson,
+  PATIENT_DIET_PLAN_CONTEXT,
+} from "../../utils/encryption.js";
 
-type PlanoAlimentarDocumento = IPlanoAlimentar & {
+type PlanoAlimentarDocumento = IPlanoAlimentarPersistido & {
   _id?: { toString(): string };
-  toObject?: () => IPlanoAlimentar & { _id?: { toString(): string } };
+  toObject?: () => IPlanoAlimentarPersistido & {
+    _id?: { toString(): string };
+  };
 };
 
-function normalizarPlanoAlimentar(planoAlimentar: IPlanoAlimentar) {
+function getPlanoDocumento(planoAlimentar: unknown) {
   const planoDocumento = planoAlimentar as PlanoAlimentarDocumento;
-  const plano =
-    typeof planoDocumento.toObject === "function"
-      ? planoDocumento.toObject()
-      : planoDocumento;
+
+  return typeof planoDocumento?.toObject === "function"
+    ? planoDocumento.toObject()
+    : planoDocumento;
+}
+
+function descriptografarPlanoAlimentar(
+  planoAlimentar: unknown,
+): IPlanoAlimentar {
+  const plano = getPlanoDocumento(planoAlimentar);
+
+  if (typeof plano?.conteudoProtegido === "string") {
+    return IPlanoAlimentarSchema.parse(
+      decryptJson<unknown>(
+        plano.conteudoProtegido,
+        PATIENT_DIET_PLAN_CONTEXT,
+      ),
+    );
+  }
+
+  return IPlanoAlimentarSchema.parse({
+    objetivoDoPlano: plano?.objetivoDoPlano,
+    observacoesGerais: plano?.observacoesGerais,
+    refeicoes: plano?.refeicoes,
+  });
+}
+
+function protegerPlanoAlimentar(planoAlimentar: IPlanoAlimentar) {
+  const planoValidado = IPlanoAlimentarSchema.parse(planoAlimentar);
+
+  return {
+    conteudoProtegido: encryptJson(
+      planoValidado,
+      PATIENT_DIET_PLAN_CONTEXT,
+    ),
+  };
+}
+
+function normalizarPlanoAlimentar(planoAlimentar: unknown) {
+  const plano = getPlanoDocumento(planoAlimentar);
+  const planoDescriptografado = descriptografarPlanoAlimentar(plano);
 
   return IPlanoAlimentarRetornoSchema.parse({
-    id: plano._id?.toString(),
-    objetivoDoPlano: plano.objetivoDoPlano,
-    observacoesGerais: plano.observacoesGerais,
-    refeicoes: plano.refeicoes,
+    id: plano?._id?.toString(),
+    ...planoDescriptografado,
   });
 }
 
 function buscarIndicePlanoAlimentar(
-  planosAlimentares: IPlanoAlimentar[],
+  planosAlimentares: unknown[],
   idPlano: string,
 ) {
   return planosAlimentares.findIndex((planoAlimentar) => {
-    const plano = planoAlimentar as PlanoAlimentarDocumento;
-    return plano._id?.toString() === idPlano;
+    const plano = getPlanoDocumento(planoAlimentar);
+    return plano?._id?.toString() === idPlano;
   });
 }
 
-function isPlanoAlimentarValido(planoAlimentar: IPlanoAlimentar) {
-  return IPlanoAlimentarSchema.safeParse(planoAlimentar).success;
+function isPlanoAlimentarValido(planoAlimentar: unknown) {
+  try {
+    descriptografarPlanoAlimentar(planoAlimentar);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 function nextPlanoAlimentarNaoEncontrado(next: NextFunction) {
@@ -54,7 +102,9 @@ function nextPlanoAlimentarNaoEncontrado(next: NextFunction) {
 
 export {
   buscarIndicePlanoAlimentar,
+  descriptografarPlanoAlimentar,
   isPlanoAlimentarValido,
   nextPlanoAlimentarNaoEncontrado,
   normalizarPlanoAlimentar,
+  protegerPlanoAlimentar,
 };
